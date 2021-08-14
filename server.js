0496d67f8031c9c1b05a1ba333d218e01fa1dbc1
Karen Marting <karenannemarting@yahoo.com>
2021-08-13 19:17:04 -0700

    able to save and retrieve notes

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require ('express');
+// const uuid = require('uuid');
 const path = require('path');
 const fs = require('fs');
 const notes = require('./db/db.json');
-const { v4: uuidv4 } = require ('uuid');
-uuidv4();
-
-// const api = require ('.public/assets/js/index.js')
-const mysql2 = require ('mysql2');
-// const { userInfo } = require('os');
+const { restart } = require('nodemon');
 
 const PORT = 3000
 const app = express();
@@ -16,17 +12,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })) 
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join
-        (__dirname, 'public/notes.html'))
+
+app.get("/api/notes", (req, res) => {
+  res.status(200).send(notes);
 })
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join
-        (__dirname, 'public/index.html'))
+app.post("/api/notes", (req, res) => {
+  notes.push(req.body);
+  notes.forEach((note, i) => {
+  note.id = i + 1;
 });
 
+  fs.writeFile('.db/db.json', JSON.stringify(notes), function() {
+    res.status(200).send(notes);
+  });
+})
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join
+        (__dirname, '/public/notes.html'))
+})
+
+app.get('/', (req, res) => {   
+    res.sendFile(path.join
+      (__dirname, '/public/index.html'))
+});
 
 app.listen(PORT, () =>
     console.log(`app listening at http://localhost${PORT}`)
@@ -47,14 +57,10 @@ app.listen(PORT, () =>
 
 
 
-// app.get('/api/notes', (req, res) => {
-//     res.json(notes);
-// });
 
-//   app.post('/api/notes', (req, res) => {
-//     notes.push(req.body);
-//     res.json(true)
-//   });
+
+
+
 
 
 
